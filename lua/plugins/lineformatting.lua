ee381bf31d921342e27d1794ebb88d22ad001031
mjoes <morten.slingsby@gmail.com>
2024-11-30 13:06:58 +0100

    plugins: Lineformatting

---
+++ lua/plugins/lineformatting.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+      indent = { char = '▏' }
+    }
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function ()
+      require("gitsigns").setup{}
+    end
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function ()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "go", "python", "nix" },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
+  }
+}
